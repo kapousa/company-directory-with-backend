320802819ea42ddbae1973282ad37474359d6285
hatem <gadallah.hatem@gmail.com>
2025-04-04 14:43:33 +0400

    test

--- src/CompanyDetails.js
+++ src/CompanyDetails.js
@@ -14,7 +14,7 @@ import {
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://admin-backend-1sev.onrender.com';
 
 const CompanyDetails = ({ username, password, navigate }) => {
   const { companyId } = useParams();
